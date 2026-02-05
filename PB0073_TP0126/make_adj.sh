43204076a0bd9b108dbe836c0b1f111e56a41423
David Kim <dekim@uw.edu>
2026-02-05 13:37:06 -0800

    Addition of calling python ahead of cmds.

--- PB0073_TP0126/make_adj.sh
+++ PB0073_TP0126/make_adj.sh
@@ -1,1 +1,1 @@
-../helper_scripts/symm_make_ss_adj.py --def_file ss_features.txt --nsub 8 --outfolder PB0073_TP0126_adj/
+python ../helper_scripts/symm_make_ss_adj.py --def_file ss_features.txt --nsub 8 --outfolder PB0073_TP0126_adj/
--- miniCXCR4/make_adj.sh
+++ miniCXCR4/make_adj.sh
@@ -1,1 +1,1 @@
-../helper_scripts/symm_make_ss_adj.py --def_file ss_features.txt --nsub 7 --outfolder miniCXCR4_adj
+python ../helper_scripts/symm_make_ss_adj.py --def_file ss_features.txt --nsub 7 --outfolder miniCXCR4_adj
